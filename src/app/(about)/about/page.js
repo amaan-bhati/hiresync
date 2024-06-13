fd3437dbefd62b4f0b83eb16e2bf2beea13faf5e
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-06-14 01:37:04 +0530

    meta data about section

--- src/app/(about)/about/page.js
+++ src/app/(about)/about/page.js
@@ -5,16 +5,16 @@ import Link from "next/link";
 
 export const metadata = {
   title: "About Me",
-  description: `Here are some details about my self.`,
+  description: `This blog site was made by Amaan, who is a web and software developer based in India`,
 };
 
 export default function About() {
   return (
     <>
       <AboutCoverSection />
-      <Skills />
+      
       <h2 className="mt-8 font-semibold text-lg md:text-2xl self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal"> 
-      Have a project in mind? Reach out to me 📞 from <Link href="/contact"  className="!underline underline-offset-2"   >here</Link> and let's make it happen.
+      Want to talk about modern web development libraries and frameworks or anything related to tech in general? Reach out to me at <Link href="https://contact.amaanbhati.tech"  className="!underline underline-offset-2"   >contact.amaanbhati.tech</Link>, let's have a quick chat.
       </h2>
     </>
   );
