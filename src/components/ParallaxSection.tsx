939579118c073dc003c4fd9c7f22acf516bbafb7
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-11-14 20:18:50 +0530

    feat: add-parallax-component

---
+++ src/components/ParallaxSection.tsx
@@ -0,0 +1,72 @@
+"use client";
+import React from "react";
+import { HeroParallax } from "./ui/hero-parallax";
+
+export function ParallaxSection() {
+  return <HeroParallax products={products} />;
+}
+export const products = [
+  {
+    title: "",
+    link: "https://hiresync-ten.vercel.app",
+    thumbnail:
+      "/girl-in-tech-svg.svg",
+  },
+  {
+    title: " ",
+    link: "https://hiresync-ten.vercel.app",
+    thumbnail:
+      "/girl-in-tech-svg.svg",
+  },
+  {
+    title: "  ",
+    link: "https://hiresync-ten.vercel.app",
+    thumbnail:
+      "/girl-in-tech-svg.svg",
+  },
+  {
+    title: "   ",
+    link: "https://hiresync-ten.vercel.app",
+    thumbnail:
+      "/girl-in-tech-svg.svg",
+  },
+  {
+    title: "     ",
+    link: "https://hiresync-ten.vercel.app",
+    thumbnail:
+      "/girl-in-tech-svg.svg",
+  },
+  {
+    title: "       ",
+    link: "https://hiresync-ten.vercel.app",
+    thumbnail:
+      "/girl-in-tech-svg.svg",
+  },
+  {
+    title: "         ",
+    link: "https://hiresync-ten.vercel.app",
+    thumbnail:
+      "/girl-in-tech-svg.svg",
+  },
+
+  {
+    title: "           ",
+    link: "https://hiresync-ten.vercel.app",
+    thumbnail:
+      "/girl-in-tech-svg.svg",
+  },
+  {
+    title: "             ",
+    link: "https://hiresync-ten.vercel.app",
+    thumbnail:
+      "/girl-in-tech-svg.svg",
+  },
+  {
+    title: "               ",
+    link: "https://hiresync-ten.vercel.app",
+    thumbnail:
+      "/girl-in-tech-svg.svg",
+  },
+
+ 
+];
