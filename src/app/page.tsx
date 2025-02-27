56ff42baafbb25af0f0a597d6135419949488e6e
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2025-02-28 03:30:42 +0530

    chore: remove unused imports to fix  production bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import { AboutSection } from "@/components/AboutSection";
 import { GeminiComponent } from "@/components/GeminiComponent";
-import { GlowingEffectSection } from "@/components/GlowingEffect";
+// import { GlowingEffectSection } from "@/components/GlowingEffect";
 // import { GlobeSection } from "@/components/GlobeSection";
 import { HeroSection } from "@/components/HeroSection";
 import { HeroVideoDialogDemo } from "@/components/HeroVideoDialogDemo";
 import { LampDemo } from "@/components/LampDemo";
-import { ParallaxSection } from "@/components/ParallaxSection";
+// import { ParallaxSection } from "@/components/ParallaxSection";
 // import { ProductsTab } from "@/components/ProductsTab";
 import { SignUpSection } from "@/components/SignUpSection";
-import { WobbleCardSection } from "@/components/WobbleCardSection";
+// import { WobbleCardSection } from "@/components/WobbleCardSection";
 // import Image from "next/image";
 
 export default function Home() {
